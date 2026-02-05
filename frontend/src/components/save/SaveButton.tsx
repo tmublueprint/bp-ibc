a05564199daa0fba28e56f7ec1a0005e894c7dfd
Tiffany Li <tiffanylii019@gmail.com>
2026-02-05 16:02:49 -0500

    Ticket 18 save button

--- frontend/src/components/save/SaveButton.tsx
+++ frontend/src/components/save/SaveButton.tsx
@@ -1,11 +1,40 @@
 function SaveButton(){
-  const dummyFunc = () => {
+  const savePage = async () => {
     console.log("Save button clicked.");
+
+    const pageElements = Array.from(document.body.querySelectorAll(
+      '[data-editable][data-editable-leaf="true"]'
+    ));
+
+    const pageName = window.location.pathname.split("/").pop() ?? "";
+    const payload = pageElements.map((element, index) => ({
+      Content: element.textContent ?? "",
+      Styling: "",  //TO DO: ADD STYLE GETTING FUNCTION
+      Section_number: index + 1,
+      Page: pageName
+    }));
+
+    try {
+      const response = await fetch('https://localhost:3001/api/sections', {
+        method: "POST", 
+        headers: { 'Content-type': 'application/json' }, 
+        body: JSON.stringify(payload),  
+      });
+
+      if (!response.ok)throw new Error("error: failed to create");
+
+      const result = await response.json();
+      console.log("created: ", result);
+      
+    } catch (error) {
+      console.error("error: ", error);
+    }
+    
+    console.log("All sections saved.");
+    console.log(pageElements)
   }
 
-  return <button onClick={dummyFunc}>Save</button>;
+  return <button onClick={savePage}>Save</button>;
 }
 
 export default SaveButton;
-
-// Replaced dummyFunc with save function when it's actually implemented
