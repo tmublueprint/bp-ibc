0356d5c8b8def5da8ccb8bd3dff6c2fd2652fab5
Vicheka Oeun <oeunvicheka95@gmail.com>
2026-03-19 23:36:38 -0400

    fix issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    css: true,
+  },
   server: {
     port: 3000,
     host: true,
