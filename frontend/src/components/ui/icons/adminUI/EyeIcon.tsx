cdf54a3615d80bb2c8851ae59b909a9fecba0070
Tristan Cheng <cheng.tristan@hotmail.com>
2026-03-25 14:58:12 -0400

    feat(icons): improved reusability by adding strokewidth prop

--- frontend/src/components/ui/icons/adminUI/EyeIcon.tsx
+++ frontend/src/components/ui/icons/adminUI/EyeIcon.tsx
@@ -4,7 +4,7 @@ interface EyeIconProps {
     color?: string;
 }
 
-export function EyeIcon({ width = 32, height = 32, color = '#000000' }: EyeIconProps) {
+export function EyeIcon({ width = 32, height = 32, color = '#1E2E5E' }: EyeIconProps) {
     return (
         <svg width={width} height={height} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1.33325 16C1.33325 16 6.66659 5.33331 15.9999 5.33331C25.3333 5.33331 30.6666 16 30.6666 16C30.6666 16 25.3333 26.6666 15.9999 26.6666C6.66659 26.6666 1.33325 16 1.33325 16Z" stroke={color} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
